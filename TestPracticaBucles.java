720c4c0cd4bc5447cdb9d6818d6704ce84c229f0
Aimar Sánchez Zabalza <asanchezzdam1@gmail.com>
2021-11-15 17:28:31 +0100

    TestPracticasBucles terminada

--- TestPracticaBucles.java
+++ TestPracticaBucles.java
@@ -24,10 +24,33 @@ public class TestPracticaBucles
      *  
      */
     public static void main(String[] args)    {
-        //TODO
-        
-        
-        
+        //Creado el teclado
+        Scanner teclado = new Scanner(System.in);
+        //Creado un objeto PracticaBucles
+        PracticaBucles bucle = new PracticaBucles();
+        //pide al usuario la cantidad máxima de aleatorios a generar
+        System.out.println("Cantidad maxima de numeros que se van a generar");
+        int aux = teclado.nextInt();
+        //valida que sea un nº positivo
+        while (aux < 0){
+            System.out.println("Por favor introduzca un numero mayor que 0");
+            aux = teclado.nextInt();
+        }
+        //muestra las estadísticas de números aleatorios
+        bucle.generarNumeros(aux);
+        //haz una pausa y borra la pantalla 
+        Utilidades.hacerPausa();
+        Utilidades.borrarPantalla();
+        //Pedir al usuario el valor de la letra "N" y gurdado en el teclado
+        System.out.println("Por favor introduzca un numero de altura para la letra N que vamos ha crear:");
+        int alt = teclado.nextInt();
+        //Validacion de la altura
+        while (3 > alt || 10 < alt){
+            System.out.println("Por favor escriba un caracter entre el 3 y el 10: ");
+            alt = teclado.nextInt();
+        }
+        // escribir la letra
+        bucle.escribirLetraN(alt);
         
     }
 }
